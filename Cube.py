ec8f0d102f1982643559d75ef0c8e1285b578d8a
pallapalla <pallapalla567@gmail.com>
2018-11-17 16:17:06 -0500

    Added method to find cubies

--- Cube.py
+++ Cube.py
@@ -72,6 +72,54 @@ class Cube:
         data = self.hypercube[minmax[0] : minmax[1], minmax[2] : minmax[3], minmax[4] : minmax[5]]
         self.hypercube[minmax[0] : minmax[1], minmax[2] : minmax[3], minmax[4] : minmax[5]] = np.rot90(data, angle / 90, axes)
 
+    """ Finds a cubie and returns its position. Cubies can be centers (len = 1), edges (len = 2) or corners (len = 3)"""
+    """ The position is an array with the same size as the cubie, containing the positions (x, y, z)"""
+    def find(self, cubie):
+        if(cubie.size == 2):
+            # edges
+            edges = [   [(2, 1, 0), (2, 0, 1)],
+                        [(1, 2, 0), (0, 2, 1)],
+                        [(3, 2, 0), (4, 2, 1)],
+                        [(2, 3, 0), (2, 4, 1)],
+                        [(1, 0, 2), (0, 1, 2)],
+                        [(3, 0, 2), (4, 1, 2)],
+                        [(0, 3, 2), (1, 4, 2)],
+                        [(4, 3, 2), (3, 4, 2)],
+                        [(2, 1, 3), (2, 0, 4)],
+                        [(1, 2, 3), (0, 2, 4)],
+                        [(3, 2, 3), (4, 2, 4)],
+                        [(2, 3, 3), (2, 4, 4)]]
+            
+            result = [] * 2
+            for edge in edges:
+                for i in range(2):
+                    color = self.hypercube[edge[i][0]][edge[i][1]][edge[i][2]]
+                    if(color in cubie):
+                        result[cubie.index(color)] = edge[i]
+                    else:
+                        break
+                else:
+                    return result
+
+        elif(cubie.size == 3):
+            # corners
+            result = [] * 3
+            for x, y, z in zip([1, 3], [1, 3], [1, 3]):
+                positions = [(x, y, 0 if z==1 else 4), (x, 0 if y==1 else 4, z), (0 if x==1 else 4, y, z)]
+                for i in range(3):
+                    color = self.hypercube[positions[i][0]][positions[i][1]][positions[i][2]]
+                    if(color in cubie):
+                        result[cubie.index(color)] = positions[i]
+                    else:
+                        break
+                else:
+                    return result
+
+        else:
+            return None
+
+
+
 
 """ tests"""
 if(__name__ == "__main__"):
