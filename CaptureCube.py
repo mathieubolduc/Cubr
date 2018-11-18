966098f7671ad2b7d09a7fc0ce1d5d17ae5f228e
Jerome Parent-Levesque <jerome-pl2@hotmail.ca>
2018-11-17 22:07:22 -0500

    WIP: Fix bugs in color retrieval and try HSV

--- CaptureCube.py
+++ CaptureCube.py
@@ -2,11 +2,14 @@ import cv2
 import numpy as np
 import time
 from grid_detection import detect_grid, make_grid
+import matplotlib.pyplot as plt
+from matplotlib.colors import hsv_to_rgb
+
 
 def show_webcam(mirror=False):
     cam = cv2.VideoCapture(0)
     cam.set(cv2.CAP_PROP_AUTO_EXPOSURE, 0.25)
-    cam.set(cv2.CAP_PROP_EXPOSURE,0.01)
+    # cam.set(cv2.CAP_PROP_EXPOSURE,0.01)
 
     delay_max = 20
     delay_cntr = 0
@@ -17,79 +20,87 @@ def show_webcam(mirror=False):
     cube_in_view = False
     image_save = []
     capture_images = True
-    new_center_dots =None
+    new_center_dots = None
+    previous_dots = []
     while capture_images:
-        ret_val, img = cam.read()
+        ret_val, img= cam.read()
 
         if mirror:
-            img = cv2.flip(img, 1)
+            img= cv2.flip(img, 1)
 
-        height, width, channels = img.shape
+        height, width, channels= img.shape
 
-        img_focus = img.copy()
-        w = int(height / 4)
-        h = w
-        x_focus = int(width / 2 - w / 2)
-        y_focus = int(height / 2-h/2)
-        cv2.rectangle(img_focus, (x_focus, y_focus), (x_focus + w, y_focus + h), (0, 255, 0), 2)
+        img_focus= img.copy()
+        w= int(height / 4)
+        h= w
+        x_focus= int(width / 2 - w / 2)
+        y_focus= int(height / 2-h/2)
+        cv2.rectangle(img_focus, (x_focus, y_focus),
+                      (x_focus + w, y_focus + h), (0, 255, 0), 2)
 
 
-        focus = img[y_focus:y_focus + h, x_focus:x_focus + w]
-        #cv2.imshow('webcam', focus)  # focus window
+        focus=img[y_focus: y_focus + h, x_focus:x_focus + w]
+        # cv2.imshow('webcam', focus)  # focus window
 
         # try to find when the cube is in optimal position
-        edges = cv2.Canny(focus, 50, 100)
-        imgray = edges
+        edges=cv2.Canny(focus, 50, 100)
+        imgray=edges
         # imgray = cv2.cvtColor(focus, cv2.COLOR_RGB2GRAY)
         # ret, thresh = cv2.threshold(imgray, 127, 255, 0)
-        blurred = cv2.GaussianBlur(imgray, (19, 19), 0)
+        blurred=cv2.GaussianBlur(imgray, (19, 19), 0)
         # thresh = cv2.threshold(blurred, 100, 255, cv2.THRESH_BINARY)[1]
-        thresh = cv2.adaptiveThreshold(blurred, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
+        thresh=cv2.adaptiveThreshold(
+            blurred, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
 
-        im2, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        im2, contours, hierarchy=cv2.findContours(
+            thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-        big_cnts = sorted(contours, key=cv2.contourArea, reverse=True)[1:12]  # get 9 biggest contours
-        img_contours = np.zeros((focus.shape))
+        big_cnts = sorted(contours, key = cv2.contourArea, reverse=True)[1: 12]  # get 9 biggest contours
+        img_contours=np.zeros((focus.shape))
         # Find the index of the largest contour
-        img_centers = img_contours.copy()
+        img_centers=img_contours.copy()
 
-        centers = []
+        centers=[]
         for cnt in big_cnts:
-            x, y, w, h = cv2.boundingRect(cnt)
-            if w/h >.85 and w/h<1.15:
-                cv2.rectangle(img_centers, (x, y), (x + w, y + h), (0, 0, 255), 2)
-                c_x = int(x + w / 2)
-                c_y = int(y + h / 2)
+            x, y, w, h=cv2.boundingRect(cnt)
+            if w/h > .85 and w/h < 1.15:
+                cv2.rectangle(img_centers, (x, y),
+                              (x + w, y + h), (0, 0, 255), 2)
+                c_x=int(x + w / 2)
+                c_y=int(y + h / 2)
                 cv2.circle(img_centers, (c_x, c_y), 2, (255, 0, 0), 2)
                 centers.append((c_x, c_y))
-        #cv2.drawContours(img_contours, contours, -1, (0, 255, 0), 3)
-        #cv2.imshow("contours", img_contours)
+        # cv2.drawContours(img_contours, contours, -1, (0, 255, 0), 3)
+        # cv2.imshow("contours", img_contours)
 
         # Grid detection
-        length, std_dev, angle1, angle2, center_point = detect_grid(centers)
-        if len(centers) > 5 and std_dev<10: # we have a face
+        length, std_dev, angle1, angle2, center_point=detect_grid(centers)
+        if len(centers) > 5 and std_dev < 10 and length > 15:  # we have a face
             delay_cntr=0
-            new_center_dots = make_grid(
+            new_center_dots=make_grid(
                 length, angle1, angle2, center_point)
             # assert len(new_center_dots)==9
             for cd in new_center_dots:
                 cv2.circle(
                     img_focus, (x_focus+int(round(cd[0])), y_focus+int(round(cd[1]))), 2, (255, 255, 255), 2)
 
-            colour_names = get_colours_pointwise(img,new_center_dots)
-            if cube_in_view == False:
+            previous_dots.append(new_center_dots)
+            if len(previous_dots) == 10 and not cube_in_view:
+                center_dots = np.mean(previous_dots, axis=0)
+                colour_names = get_colours_pointwise(img, center_dots, x_focus, y_focus)
                 print("I see a cube!")
                 print(colour_names)
-            cube_in_view = True
+                cube_in_view= True
 
-        elif delay_cntr<delay_max and new_center_dots is not None: # draw the circles if were still in delay
+        elif delay_cntr <delay_max and new_center_dots is not None: # draw the circles if were still in delay
             for cd in new_center_dots:
                 cv2.circle(
                     img_focus, (x_focus + int(round(cd[0])), y_focus + int(round(cd[1]))), 2, (255, 255, 255), 2)
-            delay_cntr+=1
+            delay_cntr += 1
 
-        elif delay_cntr>=delay_max and cube_in_view==True: #cube just left view
+        elif delay_cntr >=delay_max and cube_in_view==True: #cube just left view
             cube_in_view=False
+            previous_dots= []
             print("Cube is gone")
 
         # Peters Grid detection
@@ -147,47 +158,61 @@ def show_webcam(mirror=False):
     cv2.destroyAllWindows()
 
     time.sleep(.1)
-    cv2.imshow("saved image",image_save)
+    cv2.imshow("saved image", image_save)
 
-    #find the colours in the saved image
+    # find the colours in the saved image
     get_colours(image_save, x_mean_save, y_mean_save)
 
 
     cv2.waitKey(0)
 
 
-def get_colours_pointwise(image, points):
-    boundaries = [  # Colours in BRG
-        [75, 150,101],  # YELLOW
-        [72, 45, 155],  # RED
-        [125, 51, 15],  # BLUE
-        [140, 127, 110],  # WHITE
-        [67, 96, 11],  # GREEN
-        [70, 70, 170]  # ORANGE
+def get_colours_pointwise(image, points, x_offset, y_offset):
+    # boundaries= [  # Colours in BRG
+    #     [75, 150,101],  # YELLOW
+    #     [72, 45, 155],  # RED
+    #     [125, 51, 15],  # BLUE
+    #     [140, 127, 110],  # WHITE
+    #     [67, 96, 11],  # GREEN
+    #     [70, 70, 170]  # ORANGE
+    # ]
+    boundaries = [
+        [50 * 180/360, 63 * 255/100, 71 * 255/100],  # YELLOW
+        [8 * 180/360, 63 * 255/100, 49 * 255/100],  # RED
+        [222 * 180/360, 41 * 255/100, 48 * 255/100],  # BLUE
+        [29 * 180/360, 14 * 255/100, 68 * 255/100],  # WHITE
+        [87 * 180/360, 43 * 255/100, 45 * 255/100],  # GREEN
+        [18 * 180/360, 71 * 255/100, 76 * 255/100]  # ORANGE
     ]
     cv2.imwrite("test_vector.png", image)
     colours_list = ['Yellow', 'Red', 'Blue', 'White', 'Green', 'Orange']
     colours = np.zeros((3, 3), dtype='uint8')
 
     # convert RGB to better norm taking space
-    # _image = cv2.cvtColor(image, cv2.COLOR_RGB2LAB)
-    # boundaries_cvt = cv2.cvtColor(np.expand_dims(np.array(boundaries, dtype='uint8'), 1), cv2.COLOR_RGB2Lab)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    # plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+    plt.imshow(image)
+    plt.show()
+    # boundaries_cvt = cv2.cvtColor(np.expand_dims(np.array(boundaries, dtype='uint8'), 1), cv2.COLOR_BGR2HSV)
     # boundaries_cvt = np.squeeze(boundaries_cvt, 1)
     boundaries_cvt = boundaries
     colour_names = []
 
     for j,(x,y) in enumerate(points):
         colour_ind = 0 #default
-        x=int(round(x))
-        y=int(round(y))
+        x=int(round(x)) + x_offset
+        y=int(round(y)) + y_offset
         best_dist = np.inf
-        for i,bound in enumerate(boundaries_cvt):
-            pixels_ave = np.mean(image[x - 1:x + 1, y - 1:y + 1], axis=(0,1))
-            dist = np.linalg.norm(bound - pixels_ave)
-            if dist < best_dist:
-                best_dist = dist
-                colour_ind = i
-        colour_names.append(colours_list[colour_ind])
+        # pixels_ave = np.mean(image[x - 1:x + 1, y - 1:y + 1], axis=(0,1))
+        pixels_ave = image[x, y]
+        # for i,bound in enumerate(boundaries_cvt):
+        #     dist = np.linalg.norm(bound[:2] - pixels_ave[:2])
+        #     if dist < best_dist:
+        #         best_dist = dist
+        #         colour_ind = i
+        # colour_names.append(colours_list[colour_ind])
+        colour_names.append((pixels_ave[0] * 2, pixels_ave[1] * 100/255, pixels_ave[2] * 100/255))
+        # colour_names.append((pixels_ave[0], pixels_ave[1], pixels_ave[2]))
 
     return colour_names
 
@@ -206,7 +231,7 @@ def get_colours(image, x_mean, y_mean):
     colours = np.zeros((3,3), dtype='uint8')
 
     cv2.imwrite("test.png", image)
-    #cv2.imshow("image focus", image)
+    # cv2.imshow("image focus", image)
     _image = image.copy().astype(float)
     _image *= 255/_image.max()
     print(_image.max())
